961bd396c31e11d30ffe656ac0d17b76ef440e94
NMS <nicolas.monner@jampp.com>
2023-03-19 23:47:42 -0300

    public-spaces-cleanup: simplifica create_dashboard.

--- city_modeller/public_space.py
+++ city_modeller/public_space.py
@@ -1,5 +1,6 @@
 import json
 from functools import partial
+from collections.abc import Iterable
 
 import geopandas as gpd
 import streamlit as st
@@ -23,19 +24,62 @@ from city_modeller.utils import (
 )
 
 
-def plot_curva_pob_min_cam(distancias: gpd.GeoSeries, save: bool = False) -> None:
+plt.style.use("seaborn")
+
+
+def plot_curva_pob_min_cam(
+    distancias: gpd.GeoSeries, minutos: Iterable = range(1, 21), save: bool = False
+) -> tuple:
     """Genera curva de población vs minutos de caminata."""
-    minutos = range(1, 21)
     prop = [pob_a_distancia(distancias, minuto) for minuto in minutos]
-    fig, ax = plt.subplots(1, figsize=(8, 6))
+    fig, ax = plt.subplots(1, figsize=(24, 18))
     ax.plot(minutos, prop, "darkgreen")
     ax.set_title(
         "Porcentaje de población en CABA según minutos de caminata a un parque público"
+        "\n",
+        size=24,
     )
-    ax.set_xlabel("Minutos de caminata a un parque público")
-    ax.set_ylabel("Porcentaje de población de la CABA")
+    ax.set_xlabel("Minutos de caminata a un parque público", size=18)
+    ax.set_ylabel("Porcentaje de población de la CABA", size=18)
     if save:
-        fig.savefig("porcentajeXminutos.png")
+        fig.savefig(f"{PROJECT_DIR}/figures/porcentajeXminutos.png")
+    return fig, ax
+
+
+def plot_curva_caminata_area(
+    gdf_source: gpd.GeoSeries,
+    gdf_target: gpd.GeoDataFrame,
+    areas: Iterable = range(100, 10000, 100),
+    minutos: int = 5,
+    save: bool = False,
+) -> tuple:
+    prop = []
+    for area in areas:
+        parques_mp_area = MultiPoint(
+            [i for i in gdf_target.loc[gdf_target.loc[:, "area"] > area, "geometry"]]
+        )
+        distancia_area = partial(distancia_mas_cercano, target_points=parques_mp_area)
+        distancias = gdf_source.map(distancia_area) * 100000
+
+        prop.append(pob_a_distancia(distancias, minutos))
+
+    fig, ax = plt.subplots(1, figsize=(24, 18))
+    ax.plot(areas, prop, "darkgreen")
+    ax.set_title(
+        "Porcentaje de población en CABA a 5 minutos de caminata a un "
+        "parque público según área del parque."
+    )
+    ax.set_xlabel("Area del parque en metros")
+    ax.set_ylabel("Porcentaje de población de la CABA a 5 minutos de un parque")
+    if save:
+        fig.savefig(f"{PROJECT_DIR}/figures/porcentaje{minutos}minutos_area.png")
+    return fig, ax
+
+
+def plot_kepler(data: gpd.GeoDataFrame, config: dict):
+    map_1 = KeplerGl(height=500, data={"data": data}, config=config)
+    keplergl_static(map_1)
+    map_1.add_data(data=data, name="radios")
 
 
 def create_dashboard():
@@ -45,58 +89,20 @@ def create_dashboard():
             col1, col2 = st.columns(2)
             # Curva de población según minutos de caminata
             with col1:
-                minutos = range(1, 21)
-                prop = [
-                    pob_a_distancia(radios_p.distancia, minuto) for minuto in minutos
-                ]
-                f, ax = plt.subplots(1, figsize=(24, 18))
-
-                ax.plot(minutos, prop, "darkgreen")
-                ax.set_title(
-                    "Porcentaje de población en CABA según minutos de caminata a un "
-                    "parque público"
-                )
-                ax.set_xlabel("Minutos de caminata a un parque público")
-                ax.set_ylabel("Porcentaje de población de la CABA")
-                st.pyplot(f)
+                st.write()
+                fig, _ = plot_curva_pob_min_cam(radios_p.distancia)
+                st.pyplot(fig)
             # Curva de poblacion segun area del espacio
             with col2:
-                areas = range(100, 10000, 100)
-                prop = []
-                for area in areas:
-                    parques_mp_area = MultiPoint(
-                        [
-                            i
-                            for i in parques_p.loc[
-                                parques_p.loc[:, "area"] > area, "geometry"
-                            ]
-                        ]
-                    )
-                    distancia_area = partial(
-                        distancia_mas_cercano, target_points=parques_mp_area
-                    )
-                    distancias = radios_p.geometry.map(distancia_area) * 100000
-
-                    prop.append(pob_a_distancia(distancias, 5))
-
-                f, ax = plt.subplots(1, figsize=(24, 18))
-
-                ax.plot(areas, prop, "darkgreen")
-                ax.set_title(
-                    "Porcentaje de población en CABA a 5 minutos de caminata a un "
-                    "parque público según área del parque."
-                )
-                ax.set_xlabel("Area del parque en metros")
-                ax.set_ylabel(
-                    "Porcentaje de población de la CABA a 5 minutos de un parque"
-                )
-                st.pyplot(f)
+                fig, _ = plot_curva_caminata_area(radios_p.geometry, parques_p)
+                st.pyplot(fig)
 
         with st.container():
-            st.write("Radios censales")
-            map_1 = KeplerGl(height=500, data={"data": radios}, config=config)
-            keplergl_static(map_1)
-            map_1.add_data(data=radios, name="radios")
+            st.markdown(
+                "<h1 style='text-align: center'>Radios Censales</h1>",
+                unsafe_allow_html=True,
+            )
+            plot_kepler(radios, config)
 
 
 if __name__ == "__main__":
@@ -107,13 +113,13 @@ if __name__ == "__main__":
     parques_p = bound_multipol_by_bbox(get_public_space(), get_bbox([8]))
     parques_p["geometry"] = geometry_centroid(parques_p)
 
-    # generamos un objeto MultiPoint que contenga todos los puntos-centroides de parques
+    # Generamos un objeto MultiPoint que contenga todos los puntos-centroides de parques
     parques_multi = MultiPoint(parques_p.geometry.tolist())
     distancia_parques = partial(distancia_mas_cercano, target_points=parques_multi)
 
-    # creamos la columna distancia en ambos datasets
-    radios["distancia"] = radios_p.geometry.map(distancia_parques) * 100000
-    radios_p["distancia"] = radios_p.geometry.map(distancia_parques) * 100000
+    # Creamos la columna distancia en ambos datasets.
+    radios["distancia"] = (radios_p.geometry.map(distancia_parques) * 100000).round(3)
+    radios_p["distancia"] = (radios_p.geometry.map(distancia_parques) * 100000).round(3)
     with open(f"{PROJECT_DIR}/config/public_spaces.json") as user_file:
         config = json.loads(user_file.read())
     create_dashboard()
