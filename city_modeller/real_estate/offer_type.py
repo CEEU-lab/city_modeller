1f74ce2ce1c8cc24bb40167e1a6a62954ad0dee4
Federico <fcatalano@unsam.edu.ar>
2023-07-24 15:31:08 -0300

    real estate modeling recipes

---
+++ city_modeller/real_estate/offer_type.py
@@ -0,0 +1,91 @@
+predict_offer_class = '''real_estate_offer <- function(offer_area, prediction_method, intervals, colorsvec) {
+            # 1. data checks
+            stopifnot("Offer area must be tabular" = is(offer_area, "data.frame"))
+            stopifnot("Prediction must be linear, orthogonal or spline" = is.character(
+                prediction_method)
+                )
+            stopifnot("Specify the max number of intervals" = is.numeric(intervals))
+            stopifnot("Colors must be a vector of char" = is.vector(colorsvec))
+            
+            # 2. Loads Point geoms
+            require("sf")
+            require("dplyr")
+            # Convert csv to simple feat object
+            ppp_caba <- offer_area %>%
+            st_as_sf(coords = c("lon", "lat"), crs = 4326)
+            ppp_coords <- st_coordinates(ppp_caba)
+            ppp_data <- ppp_caba %>% mutate(lon = ppp_coords[, "X"],
+                                            lat = ppp_coords[, "Y"],
+                                            tipo = as.factor(tipo_agr)) %>%
+                dplyr::select(lat, lon, tipo)
+            # last factor level in alphabetical order is the target class
+            target_label <- tail(levels(ppp_data$tipo),1)
+            print(paste("Target class: ", target_label))
+
+            # 3. logistic adjustment (intensity func)
+            require(splines)
+            if (prediction_method == "linear") {
+                #linear
+                logistic_adj <- glm(tipo ~ lon + lat, data = ppp_data, family = "binomial")
+            } else if (prediction_method == "orthogonal") {
+                # Orthogonal Polynomials
+                logistic_adj <- glm(tipo ~ poly(lon, 3) * poly(lat, 3),
+                data = ppp_data, family = "binomial")
+            } else if (prediction_method == "splines") {
+                # Polynomial spline
+                logistic_adj <- glm(tipo ~ bs(lon, 4) * bs(lat, 4),
+                data = ppp_data, family = "binomial")
+            }
+
+            # 4. grid predictions
+            cant <- 100
+            grid_canvas <- expand.grid(
+                lon = seq(
+                min(ppp_data$lon),
+                max(ppp_data$lon),
+                length.out = cant),
+                lat = seq(min(ppp_data$lat),
+                max(ppp_data$lat),
+                length.out = cant))
+            # require("terra")
+            pred <- predict(logistic_adj, newdata = grid_canvas, type = "response")
+            summary(pred)
+
+            # Empty raster
+            require(raster)
+            rowxcol <- cant
+            raster_canvas <- raster(
+                nrows = rowxcol, ncols = rowxcol,
+                xmn = min(ppp_data$lon), xmx = max(ppp_data$lon),
+                ymn = min(ppp_data$lat), ymx = max(ppp_data$lat)
+            )
+            # Rasterize prediction
+            raster_pred <- raster::rasterize(grid_canvas,
+            raster_canvas, field = pred, fun = median)
+            sf_collection <- st_union(ppp_data)
+            study_area <- st_convex_hull(sf_collection)
+            raster_mask <- raster::mask(raster_pred, as_Spatial(study_area))
+            raster_vals <- raster::values(raster_mask)
+
+            # 5. Plot prediction results
+            require(tmap)
+            require(leaflet)
+
+            cat_breaks <- seq(min(raster_vals, na.rm = TRUE),
+            max(raster_vals, na.rm = TRUE), length.out = intervals)
+
+            col_bins <- colorBin(
+                palette = colorsvec,
+                bins = cat_breaks,
+                domain = cat_breaks,
+                na.color = "#00000000")
+                    
+            outmap <- leaflet(width = 900, height = 400) %>%
+                addTiles() %>%
+                addProviderTiles(providers$CartoDB.Positron) %>%
+                addRasterImage(raster_mask, opacity = 0.75, colors = colorsvec) %>%
+                addLegend(pal = col_bins, values = cat_breaks)
+            
+            require(htmlwidgets)
+            saveWidget(widget = outmap, file = "mymap.html")
+            }'''
---
+++ city_modeller/real_estate/utils.py
@@ -0,0 +1,23 @@
+def density_agg_cat(x):
+    btypes = {
+        'high-density-types': ['Departamento'],
+        'low-density-types': ['Casa', 'PH']
+        }
+    if x in btypes['high-density-types']:
+        return 'high-density-types'
+    elif x in btypes['low-density-types']:
+        return 'low-density-types'
+    else:
+        return 'other'
+                    
+def residential_agg_cat(x):
+    btypes = {
+        'residential-types': ['Casa', 'PH', 'Departamento'],
+        'non-residential-types':['Oficina', 'Depósito', 'Local' 'comercial']
+        }
+    if x in btypes['high-density-types']:
+        return 'high-density-types'
+    elif x in btypes['low-density-types']:
+        return 'low-density-types'
+    else:
+        return 'other'
