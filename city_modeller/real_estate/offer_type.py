0e046db02f821300a6e39c2262716f363f94b53a
Federico <fcatalano@unsam.edu.ar>
2023-10-16 18:10:55 -0300

    commit 260e640 + remove sf r-package

--- city_modeller/real_estate/offer_type.py
+++ city_modeller/real_estate/offer_type.py
@@ -2,29 +2,24 @@ from rpy2 import robjects as ro
 from rpy2.robjects import pandas2ri
 from rpy2.robjects import conversion, default_converter
 
+import geopandas as gpd
+import rasterio
+from rasterio.features import shapes
+
 
 # TODO: docstring + how terra predicts over the grid (wiki)
-predict_offer_class = """real_estate_offer <- function(offer_area, prediction_method, intervals, colorsvec, dir) {
+predict_offer_class = """real_estate_offer <- function(offer_area, prediction_method, dir) {
             # 1. data type checks
             stopifnot("Offer area must be tabular" = is(offer_area, "data.frame"))
             stopifnot("Prediction must be linear, orthogonal or spline" = is.character(
                 prediction_method)
                 )
-            stopifnot("Specify the max number of intervals" = is.numeric(intervals))
-            stopifnot("Colors must be a vector of char" = is.vector(colorsvec))
             stopifnot("The path name must be a string of char with destination output" = is.character(
                 dir))
             
-            # 2. Loads Point geoms
-            require("sf")
+            # 2. Loads Point coords
             require("dplyr")
-            # Convert csv to simple feat object
-            ppp_caba <- offer_area %>%
-            st_as_sf(coords = c("lon", "lat"), crs = 4326)
-            ppp_coords <- st_coordinates(ppp_caba)
-            ppp_data <- ppp_caba %>% mutate(lon = ppp_coords[, "X"],
-                                            lat = ppp_coords[, "Y"],
-                                            tipo = as.factor(tipo_agr)) %>%
+            ppp_data <- offer_area %>% mutate(tipo = as.factor(tipo_agr)) %>%
                 dplyr::select(lat, lon, tipo)
             # last factor level in alphabetical order is the target class
             target_label <- tail(levels(ppp_data$tipo),1)
@@ -75,39 +70,11 @@ predict_offer_class = """real_estate_offer <- function(offer_area, prediction_me
             # Rasterize prediction
             raster_pred <- raster::rasterize(grid_canvas,
             raster_canvas, field = pred, fun = median)
-            sf_collection <- st_union(ppp_data)
-            study_area <- st_convex_hull(sf_collection)
-            raster_mask <- raster::mask(raster_pred, as_Spatial(study_area))
-            raster_vals <- raster::values(raster_mask)
-
-            # 5. Plot prediction results
-            require(tmap)
-            require(leaflet)
-
-            cat_breaks <- seq(min(raster_vals, na.rm = TRUE),
-            max(raster_vals, na.rm = TRUE), length.out = intervals)
-
-            col_bins <- colorBin(
-                palette = colorsvec,
-                bins = cat_breaks,
-                domain = cat_breaks,
-                na.color = "#00000000")
-                    
-            # export result
-            outmap <- leaflet(width = 600, height = 400,
-                options = leafletOptions(zoomControl = TRUE,
-                                    minZoom = 10, maxZoom = 15)) %>%
-                addTiles() %>%
-                addProviderTiles(providers$CartoDB.Positron) %>%
-                addRasterImage(raster_mask, opacity = 0.75, colors = colorsvec) %>%
-                addLegend(pal = col_bins, values = cat_breaks)
             
-            require(htmlwidgets)
-            saveWidget(widget = outmap, file = dir)
-            }"""
+            writeRaster(raster_pred,dir,format="GTiff", overwrite=TRUE) }"""
 
 
-def offer_type_predictor_wrapper(df, path) -> None:
+def offer_type_predictor_wrapper(df, geom, path) -> None:
     """
     Python wrapper to run the R function predict_offer_class.
 
@@ -115,6 +82,7 @@ def offer_type_predictor_wrapper(df, path) -> None:
     -----------
     df : pd.DataFrame
         Real Estate Offer with adjusted classes for prediction
+    geom: 
     path : str
         Source route of the predicted output (html widget)
 
@@ -124,6 +92,8 @@ def offer_type_predictor_wrapper(df, path) -> None:
         Writes leaflet html widget
     """
 
+    json_name = path.split("/")[-1].split(".")[0]
+    
     with conversion.localconverter(default_converter):
         # loads pandas as data.frame r object
         with (ro.default_converter + pandas2ri.converter).context():
@@ -132,11 +102,29 @@ def offer_type_predictor_wrapper(df, path) -> None:
         # parameters
         # TODO: use histogram to chek the observed distribution of the target class?
         prediction_method = "splines"  # DENSITY FUNCTION.
-        intervals = 10
-        colorsvec = ro.StrVector(["lightblue", "yellow", "purple"])
 
         # predict offer type
         ro.r(predict_offer_class)
         predominant_offer = ro.globalenv["real_estate_offer"]
-        # exports html result
-        predominant_offer(r_from_pd_df, prediction_method, intervals, colorsvec, path)
+        # exports tif result
+        predominant_offer(r_from_pd_df, prediction_method, path)
+
+    mask = None
+    with rasterio.Env():
+        with rasterio.open(path) as src:
+            image = src.read(1) # first band
+            results = (
+                {'properties': {'raster_val': v}, 'geometry': s}
+                for i, (s, v) in enumerate(
+                    shapes(image, mask=mask, transform=src.transform))
+            )
+            
+    src.close()
+    gpd_polygonized_raster = gpd.GeoDataFrame.from_features(list(results)) 
+    gpd_polygonized_raster = gpd_polygonized_raster.set_crs("epsg:4326")
+    #import streamlit as st
+    #gpd_polygonized_raster_ = gpd_polygonized_raster.clip(geom)
+    #st.write(gpd_polygonized_raster_)
+    #st.write(geom) # HACER UN CONVEX HULL CON ESTA geom
+    gpd_polygonized_raster.raster_val = round(gpd_polygonized_raster.raster_val, 2)
+    gpd_polygonized_raster.to_file(f"./real_estate/results/{json_name}.geojson", driver='GeoJSON')
